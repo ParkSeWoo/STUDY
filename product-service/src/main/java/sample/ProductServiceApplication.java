4dd227d199cc996718fb32ac95f5efe14db135eb
sewoo <sewooimda@nate.com>
2019-04-13 23:05:45 +0900

    [feature-20190413] - service modify

--- product-service/src/main/java/sample/ProductServiceApplication.java
+++ product-service/src/main/java/sample/ProductServiceApplication.java
@@ -3,7 +3,6 @@ package sample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
