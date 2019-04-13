9d39c9426acff15f34749b47f5f1feac506bdccd
sewoo <sewooimda@nate.com>
2019-04-13 18:46:08 +0900

    [feature-20190413] - zuul 서버 추가

---
+++ api-zuul-gateway/src/main/java/sample/ApiZuulGatewayServerApplication.java
@@ -0,0 +1,17 @@
+package sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
+class ApiZuulGatewayServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiZuulGatewayServerApplication.class, args);
+
+    }
+}
+
