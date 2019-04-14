78179f63c1f5cd6912af677ba45d8c1047cacf8a
sewoo <sewooimda@nate.com>
2019-04-14 12:08:37 +0900

    [feature-20190414] - spring cloud gateway add

--- spring-cloud-gateway/src/main/java/sample/SpringCloudGatewayServerApplication.java
+++ spring-cloud-gateway/src/main/java/sample/SpringCloudGatewayServerApplication.java
@@ -13,13 +13,16 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
+@RestController
 @SpringBootApplication
 public class SpringCloudGatewayServerApplication {
 
     @RequestMapping("/hystrixfallback")
-    public String hystrixfallback() {
-        return "This is a fallback";
+    public Mono<String> hystrixfallback() {
+        return Mono.just("fallback");
     }
 
     @Bean
@@ -27,28 +30,21 @@ public class SpringCloudGatewayServerApplication {
         //@formatter:off
         return builder.routes()
                 .route("path_route", r -> r.path("/get")
-                        .uri("http://httpbin.org"))
-                /*.route("host_route", r -> r.host("*.myhost.org")
-                        .uri("http://httpbin.org"))
-                .route("rewrite_route", r -> r.host("*.rewrite.org")
-                        .filters(f -> f.rewritePath("/foo/(?<segment>.*)",
-                                "/${segment}"))
-                        .uri("http://httpbin.org"))
-                .route("hystrix_route", r -> r.host("*.hystrix.org")
-                        .filters(f -> f.hystrix(c -> c.setName("slowcmd")))
-                        .uri("http://httpbin.org"))
-                .route("hystrix_fallback_route", r -> r.host("*.hystrixfallback.org")
-                        .filters(f -> f.hystrix(c -> c.setName("slowcmd").setFallbackUri("forward:/hystrixfallback")))
-                        .uri("http://httpbin.org"))*/
+                  .filters(f -> f.addRequestHeader("Hello", "World"))
+                  .uri("http://httpbin.org"))
+                .route(p -> p
+                        .host("*.hystrix.com")
+                        .filters(f -> f.hystrix(config -> config.setName("mycmd")
+                         .setFallbackUri("forward:/hystrixfallback")))
+                        .uri("http://httpbin.org:80"))
                 .route("limit_route", r -> r
                         .host("*.limited.org").and().path("/anything/**")
                         .filters(f -> f.requestRateLimiter(c -> c.setRateLimiter(redisRateLimiter())))
                         .uri("http://httpbin.org"))
-                /*.route("websocket_route", r -> r.path("/echo")
-                        .uri("ws://localhost:9000"))*/
                 .build();
         //@formatter:on
     }
+
 
     /*
     * 사용자당 요청된 초당 요청 수
